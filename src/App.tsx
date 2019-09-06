441dd38b547bd0decff40aa0e75b74c4545c0b4b
Julio Berina <julioberina@gmail.com>
2019-09-05 23:58:10 -0700

    Remove app defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-const App: React.FC = () => {
+type HelloProps = {
+  person: string;
+};
+
+type Props = {};
+
+type State = {
+  appName: string;
+};
+
+const Hello: React.FC<HelloProps> = ({ person }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <strong>Hello {person}. How are you</strong>
+  )
 }
 
-export default App;
+export default class App extends React.Component<Props, State> {
+  
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      appName: 'Real Estate App'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.appName}</h1>
+        <Hello person="Julio"></Hello>
+      </div>
+    )
+  }
+
+}
