46672bd5c978eedc2c5ad9354d16e93deb827c63
Julio Berina <julioberina@gmail.com>
2019-09-22 00:47:11 -0700

    Finish styling header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -12,12 +12,12 @@ export default class Header extends React.Component<HeaderProps, HeaderState> {
     render() {
         return (
             <header>
-                <div>Logo</div>
+                <div className="logo">Logo</div>
 
                 <nav>
-                    <a href="#">create ads</a>
-                    <a href="#">about us</a>
-                    <a href="#">log in</a>
+                    <a href="#">Create Ads</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Log In</a>
                     <a href="#" className="register-btn">Register</a>
                 </nav>
             </header>
